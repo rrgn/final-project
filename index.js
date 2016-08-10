daf96bf835b1cdb9a9bcca76dd4e16af77e566d8
Regan <regs.co@gmail.com>
2016-08-10 17:42:40 -0400

    encrypting data

--- index.js
+++ index.js
@@ -1,37 +1,89 @@
 var express = require('express');
-// var crypto = require('crypto');
+var mongoose = require('mongoose');
+const crypto = require('crypto');
+var session = require('express-session');
 var app = express();
 var bodyParser = require('body-parser');
-var masPass;
+var User = require('./user');
+// var encPass;
 
-function catchpass(pass) {
-  masPass = pass;
-  console.log('inside catchpass: ', masPass);
-  return masPass;
-}
+mongoose.connect('mongodb://localhost/FlatPass');
 
-
-// const crypto = require('crypto');
-// const cipher = crypto.createCipher('aes192', mPass);
+// function catchpass(pass) {
+//   encPass = pass;
+//   console.log('inside catchpass: ', encPass);
+//   return encPass;
+// }
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
-
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // catch password
 app.post('/password', function(request, res) {
+  console.log('this is request.body', request.body);
   var mPass = request.body.pass;
-  // console.log('backend post:', mPass);
-  catchpass(mPass);
+  // console.log(mPass);
+  // var mPassString = JSON.stringify(mPass);
+  // const cipher = crypto.createCipher('aes192', mPassString);
+  // var encrypted = cipher.update('the bird is the word','utf8', 'hex');
+  // encrypted += cipher.final('hex');
+  // console.log('encrypted: ', encrypted);
+  // catchpass(mPass);
+  // var user = new User({
+  //   nameNpass: encrypted
+  // });
+  // user.save(function(err) {
+  //   if (err) {
+  //     console.log('error in save: ', err.message);
+  //     return;
+  //   }
+  // });
   res.send('ok');
 });
 
 // catch data
 app.post('/data', function(request, res) {
   var info = request.body;
+  console.log(info);
   var stringInfo = JSON.stringify(info);
-  // console.log('info sent: ', info, stringInfo);
-  console.log(masPass);
+  // const cipher = crypto.createCipher('aes192', encPass);
+  // var encryptedData = cipher.update(stringInfo, 'utf8', 'hex');
+  // console.log('encrypted info sent: ', stringInfo, encryptedData);
+  // var data = new User({
+  //   logins: encryptedData
+  // });
+  // data.save(function(err) {
+  //   if(err) {
+  //     console.log('error in save: ', err);
+  //     return;
+  //   }
+  //   res.send('ok');
+  // });
+});
+
+//retrieve user data
+app.get('/word', function(request, res) {
+  var key = request.body;
+  console.log('this is the key:', key);
+  User.find(function(err, users) {
+    if(err) {
+      console.log(err.message);
+      return;
+    }
+    var userInfo = users[0].logins;
+    console.log('Users: ', users);
+    console.log('key: ', typeof(key));
+    // const decipher = crypto.createDecipher('aes192', encPass);
+    // var decrypted = decipher.update(info, 'hex', 'utf8');
+    // decrypted += decipher.final('utf8');
+    // console.log(decrypted);
+  });
   res.send('ok');
 });
 
--- public/script.js
+++ public/script.js
@@ -3,15 +3,22 @@ var app = angular.module('app', []);
 //master password controller
 app.controller('MainController', function($scope, savePass, sendData) {
   $scope.login = function() {
-    var pword = $scope.password;
-    savePass.savePassword(pword, function(password) {
+    var loginData = {
+      email: $scope.email,
+      mPassword: $scope.password
+    };
+    // var pword = $scope.password;
+    savePass.savePassword(loginData, function() {
     });
   };
   $scope.saveInfo = function() {
     var userData = {
-      website: $scope.website,
-      username: $scope.username,
-      acctPassword: $scope.acctPassword
+      mPassword: $scope.password,
+      data: {
+        website: $scope.website,
+        username: $scope.username,
+        acctPassword: $scope.acctPassword
+      }
     };
     sendData.userInfo(userData);
     console.log('clicked the save button', userData);
@@ -21,9 +28,13 @@ app.controller('MainController', function($scope, savePass, sendData) {
 
 app.factory('savePass', function($http) {
   return {
-    savePassword: function(password, callback) {
-      console.log(password);
-      var pass = {pass: password};
+    savePassword: function(loginData, callback) {
+      // console.log(password);
+      var pass = {
+        email: loginData.email,
+        pass: loginData.mPassword
+      };
+      console.log(pass);
       $http.post('/password', pass)
       .success(function(data) {
         callback(data);
