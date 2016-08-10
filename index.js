2529ce2fb95dc5dd3e51d81d30cf6533e512cb09
Regan <regs.co@gmail.com>
2016-08-10 10:28:01 -0400

    added functionality

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ var express = require('express');
 // var crypto = require('crypto');
 var app = express();
 var bodyParser = require('body-parser');
+var masPass;
+
+function catchpass(pass) {
+  masPass = pass;
+  console.log('inside catchpass: ', masPass);
+  return masPass;
+}
+
 
 // const crypto = require('crypto');
 // const cipher = crypto.createCipher('aes192', mPass);
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
-// var cert1 = new crypto.Certificate();
-// console.log(cert1);
+
 
 // catch password
 app.post('/password', function(request, res) {
   var mPass = request.body.pass;
-  console.log('backend post:', mPass);
+  // console.log('backend post:', mPass);
+  catchpass(mPass);
   res.send('ok');
 });
 
@@ -22,7 +30,8 @@ app.post('/password', function(request, res) {
 app.post('/data', function(request, res) {
   var info = request.body;
   var stringInfo = JSON.stringify(info);
-  console.log('info sent: ', info, stringInfo);
+  // console.log('info sent: ', info, stringInfo);
+  console.log(masPass);
   res.send('ok');
 });
 
